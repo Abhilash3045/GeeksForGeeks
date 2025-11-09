5b50c479996a851df157c8a888a20dacfeea6f3f
Abhilash <dopatiabhilash@gmail.com>
2025-11-09 17:05:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Test if tuple is distinct/test-if-tuple-is-distinct.py
@@ -0,0 +1,7 @@
+#User function Template for python3
+arr = tuple(map(int, input().split()))
+print(len(arr)==len(set(arr)))
+########### Write your code below ###############
+# Print "True" if all elements of tuple are different, otherwise print "False"
+
+########### Write your code above ###############
